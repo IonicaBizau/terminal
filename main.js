8e8fab0af499a9a090ec951a244143f80d3dd0d7
Ionica Bizau <bizauionica@yahoo.com>
2013-06-07 16:44:56 +0300

    Commented the code because the current version of mono doesn't support web sockets.

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 module.exports = function (config) {
-    // Requiring the server file.
-    this.link("initialize", function (err) {
-        if (err) { return; }
-
-        ;(function() {
-            return setTimeout(function() {
-                var term = new Terminal(80, 24)
-                  , socket = io.connect();
-
-                socket.on('connect', function() {
-                    term.on('data', function(data) {
-                        socket.emit('data', data);
-                    });
-
-                    term.on('title', function(title) {
-                        document.title = title;
-                    });
-
-                    term.open();
-
-                    socket.on('data', function(data) {
-                        term.write(data);
-                    });
-                });
-            }, 1000);
-        }).call(this);
-    });
+//    // Requiring the server file.
+//    this.link("initialize", function (err) {
+//        if (err) { return; }
+//
+//        ;(function() {
+//            return setTimeout(function() {
+//                var term = new Terminal(80, 24)
+//                  , socket = io.connect();
+//
+//                socket.on('connect', function() {
+//                    term.on('data', function(data) {
+//                        socket.emit('data', data);
+//                    });
+//
+//                    term.on('title', function(title) {
+//                        document.title = title;
+//                    });
+//
+//                    term.open();
+//
+//                    socket.on('data', function(data) {
+//                        term.write(data);
+//                    });
+//                });
+//            }, 1000);
+//        }).call(this);
+//    });
 };
--- operations.js
+++ operations.js
@@ -5,44 +5,44 @@ var pty = require("pty.js");
 // Operations    //
 ///////////////////
 exports.initialize = function (link) {
-    // Open Terminal
-    var buff = [], socket, term;
-
-    var term = pty.fork(process.env.SHELL || 'sh', [], {
-        name: 'xterm',
-        cols: 80,
-        rows: 24,
-        cwd: process.env.HOME
-    });
-
-    term.on('data', function(data) {
-      return !socket
-        ? buff.push(data)
-        : socket.emit('data', data);
-    });
-
-    // Sockets
-    io = Io.listen(server);
-
-    io.configure(function() {
-      io.disable('log');
-    });
-
-    io.sockets.on('connection', function(sock) {
-      socket = sock;
-
-      socket.on('data', function(data) {
-        term.write(data);
-      });
-
-      socket.on('disconnect', function() {
-        socket = null;
-      });
-
-      while (buff.length) {
-        socket.emit('data', buff.shift());
-      }
-    });
+//    // Open Terminal
+//    var buff = [], socket, term;
+//
+//    var term = pty.fork(process.env.SHELL || 'sh', [], {
+//        name: 'xterm',
+//        cols: 80,
+//        rows: 24,
+//        cwd: process.env.HOME
+//    });
+//
+//    term.on('data', function(data) {
+//      return !socket
+//        ? buff.push(data)
+//        : socket.emit('data', data);
+//    });
+//
+//    // Sockets
+//    io = Io.listen(server);
+//
+//    io.configure(function() {
+//      io.disable('log');
+//    });
+//
+//    io.sockets.on('connection', function(sock) {
+//      socket = sock;
+//
+//      socket.on('data', function(data) {
+//        term.write(data);
+//      });
+//
+//      socket.on('disconnect', function() {
+//        socket = null;
+//      });
+//
+//      while (buff.length) {
+//        socket.emit('data', buff.shift());
+//      }
+//    });
 
     link.send(200);
 };
