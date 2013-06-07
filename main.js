8e07999fd7e5a7c25a262399ea57a037b76071a2
Ionica Bizau <bizauionica@yahoo.com>
2013-06-07 15:59:32 +0300

    Added web-socket scripts.

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 module.exports = function (config) {
-    // ...
+    ;(function() {
+        return setTimeout(function() {
+            var term = new Terminal(80, 24)
+              , socket = io.connect();
+
+            socket.on('connect', function() {
+                term.on('data', function(data) {
+                    socket.emit('data', data);
+                });
+
+                term.on('title', function(title) {
+                    document.title = title;
+                });
+
+                term.open();
+
+                socket.on('data', function(data) {
+                    term.write(data);
+                });
+            });
+        }, 1000);
+    }).call(this);
 };
--- operations.js
+++ operations.js
@@ -1,5 +1,53 @@
-// require tty
+var io = require("socket.io");
+var pty = require("pty.js");
 
+///////////////////
+// Open Terminal //
+///////////////////
+var buff = [], socket, term;
+
+var term = pty.fork(process.env.SHELL || 'sh', [], {
+    name: 'xterm',
+    cols: 80,
+    rows: 24,
+    cwd: process.env.HOME
+});
+
+term.on('data', function(data) {
+  return !socket
+    ? buff.push(data)
+    : socket.emit('data', data);
+});
+
+///////////////////
+// Sockets       //
+///////////////////
+io = io.listen(server);
+
+io.configure(function() {
+  io.disable('log');
+});
+
+io.sockets.on('connection', function(sock) {
+  socket = sock;
+
+  socket.on('data', function(data) {
+    term.write(data);
+  });
+
+  socket.on('disconnect', function() {
+    socket = null;
+  });
+
+  while (buff.length) {
+    socket.emit('data', buff.shift());
+  }
+});
+
+///////////////////
+// Operations    //
+///////////////////
+// TODO Do we need this 'run' operation?
 exports.run = function (link) {
     if (!link.data) { return link.send(400, "Missing data."); }
 
